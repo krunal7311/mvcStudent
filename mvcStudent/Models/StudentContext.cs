f583bcf4a98b567e20eadfe97c1d438628571d64
Unknown <krunalbodiwala7311@icloud.com>
2018-05-17 09:11:20 +1200

    Configured to use SQLite

---
+++ mvcStudent/Models/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvcStudent.Models
+{
+    public class StudentContext: DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<mvcStudent.Models.Student> Movie { get; set; }
+        public DbSet<mvcStudent.Models.Course> Course { get; set; }
+        public DbSet<mvcStudent.Models.Enrollment> Enrollment { get; set; }
+
+    }
+}
--- mvcStudent/Startup.cs
+++ mvcStudent/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using mvcStudent.Models;
+
 
 namespace mvcStudent
 {
@@ -21,6 +24,8 @@ namespace mvcStudent
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StudentContext>(options =>
+                 options.UseSqlite("Data Source=mvcStudent.db"));
             services.AddMvc();
         }
 
